db13e27d5d5bc5b8774452fc533ebb269cf0201c
satawait <xingbangl@gmail.com>
2022-07-05 23:24:23 +0800

    Initial commit

---
+++ src/MyButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+defineProps({
+  /* 
+  When someone installs and imports our MyButton.vue component form our library,
+  Volar should offer "calamari" and "tuna" autocompletions for it
+  */
+  seafood: {type: String as PropType<"calamari" | "tuna">, required: true} 
+})
+</script>
+<template>
+<button>
+  I'm a button, my prop is {{ seafood }}
+</button>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'MyLib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+})
