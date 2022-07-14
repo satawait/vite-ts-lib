51b5403fb94a61a9a1b8f41488248b2db566d92d
20201206-013040\Administrator <xingbangl@gmail.com>
2022-07-14 22:02:50 +0800

    study

---
+++ src/examples/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <MyButton seafood="tuna"/>
+        {{ num }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { MyButton, sum } from 'vite-ts-lib'
+const num = ref(sum(1, 2, 3, 4, 5))
+</script>
+
+<style scoped>
+
+</style>
